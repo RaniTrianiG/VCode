002e204c17a81333cc74a62e9d2b793c81ddff93
RaniTrianiG <trianirani71@gmail.com>
2017-09-06 20:01:49 +0700

    Baru

--- multiple_login/Login.cs
+++ multiple_login/Login.cs
@@ -18,31 +18,9 @@ namespace multiple_login
             InitializeComponent();
         }
 
-        int login;
         private void button1_Click(object sender, EventArgs e)
         {
-            string sql = "select uname, pwd from account where uname = '" + txtUsername.Text + "' and pwd = '" + txtPassword.Text + "'";
-            DataTable dt = new DataTable();
-            Module db = new Module();
-            dt = db.BukaTable(sql);
-            if(dt.Rows.Count > 0)
-            {
-                MessageBox.Show("Login Berhasil");
-                string roleee = "select role from account where uname = '" + txtUsername.Text + "'"; 
-                DataTable dt2 = new DataTable();
-                dt2 = db.BukaTable(roleee);
-                Module.rolee = dt2.Rows[0][0].ToString();
-                Menu r = new Menu();
-                r.Show();
-                this.Hide();
             }
             
-            else
-            {
-                MessageBox.Show("Login Gagal");
-                txtUsername.Text = "";
-                txtPassword.Text = "";
-            }
         }
     }
-}
